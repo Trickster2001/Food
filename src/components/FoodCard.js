375479fdb353907524830817e22b7ecc251e4252
impact@gmail.com <keyurmurkar@gmail.com>
2023-08-29 18:29:20 +0530

    minor design changes

--- src/components/FoodCard.js
+++ src/components/FoodCard.js
@@ -8,18 +8,18 @@ const FoodCard = ({chicken}) => {
     <>
     
     <a href="/recipe/123">Hello</a>
-      <div className='relative grid sm:grid-cols-2 bg-black '>
+      <div className='relative grid sm:grid-cols-2  '>
         {chicken.map((res)=>{
           return (
-            <div key={res.id}>
-              <section className='bg-red-400'>
+            <div key={res.id} className=''>
+              <section className=''>
                 <Link to={`/recipe/${res.id}`} >
                   <img className='w-full p-4' src={res.image} alt={res.title} />
                 </Link>
               </section>
-              <section className='bg-blue-200'>
+              <section className='bg-black mx-4'>
               <Link to={`/recipe/${res.id}`} >
-                <h2 className='font-bold text-2xl p-2'>{res.title}</h2>
+                <h2 className='font-bold text-white text-center text-2xl  p-4'>{res.title}</h2>
               </Link>
               </section>
             </div>
